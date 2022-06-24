cd9d1f8d781efdb47f031c99c1c173c680547873
jsthomas <jsthomas1288@gmail.com>
2022-06-24 08:23:46 -0500

    css animations, different color scheme, different links for some projects

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const html = document.querySelector("html");
 const selector = document.querySelector("#themes");
+const getThemeString = localStorage.getItem("theme");
+
+const savedTheme = JSON.parse(getThemeString);
+if (savedTheme === "dark") {
+  html.classList.remove("light");
+} else if (savedTheme === "light") {
+  html.classList.add("light");
+}
 
 selector.addEventListener("change", function (e) {
   let theme = e.target.value;
   if (theme === "dark") {
+    html.classList.add("transition");
     html.classList.remove("light");
-    // console.log("yes");
+    const saveTheme = JSON.stringify(theme);
+    localStorage.setItem("theme", saveTheme);
   } else if (theme === "light") {
+    html.classList.add("transition");
     html.classList.add("light");
-    // console.log("no");
+    const saveTheme = JSON.stringify(theme);
+    localStorage.setItem("theme", saveTheme);
   }
 });
